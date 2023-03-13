81ae98523bdc60cea747f51ff138c696e126b57a
Jevison Archibong <jevison7x@yahoo.com>
2023-03-13 11:50:09 +0100

    Completed company service app

---
+++ src/java/com/bizblock/company/CompanyStock.java
@@ -0,0 +1,121 @@
+/*
+ * Copyright (c) 2018, Xyneex Technologies. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * You are not meant to edit or modify this source code unless you are
+ * authorized to do so.
+ *
+ * Please contact Xyneex Technologies, #1 Orok Orok Street, Calabar, Nigeria.
+ * or visit www.xyneex.com if you need additional information or have any
+ * questions.
+ */
+package com.bizblock.company;
+
+import static com.bizblock.company.CompanyStock.COMPANY_STOCKS;
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author Jevison7x
+ * @since Mar 13, 2023 8:56:36 AM
+ */
+@Entity
+@Table(name = COMPANY_STOCKS)
+public class CompanyStock implements Serializable
+{
+    private static final long serialVersionUID = 1L;
+    @Id
+    private String symbol;
+    private String name;
+    private String logo;
+    private String currency;
+    private String currencyName;
+    private double sharePrice;
+    private int numberOfShares;
+
+    public CompanyStock()
+    {
+    }
+
+    public String getSymbol()
+    {
+        return symbol;
+    }
+
+    public void setSymbol(String symbol)
+    {
+        this.symbol = symbol;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public String getLogo()
+    {
+        return logo;
+    }
+
+    public void setLogo(String logo)
+    {
+        this.logo = logo;
+    }
+
+    public String getCurrency()
+    {
+        return currency;
+    }
+
+    public void setCurrency(String currency)
+    {
+        this.currency = currency;
+    }
+
+    public String getCurrencyName()
+    {
+        return currencyName;
+    }
+
+    public void setCurrencyName(String currencyName)
+    {
+        this.currencyName = currencyName;
+    }
+
+    public double getSharePrice()
+    {
+        return sharePrice;
+    }
+
+    public void setSharePrice(double sharePrice)
+    {
+        this.sharePrice = sharePrice;
+    }
+
+    public int getNumberOfShares()
+    {
+        return numberOfShares;
+    }
+
+    public void setNumberOfShares(int numberOfShares)
+    {
+        this.numberOfShares = numberOfShares;
+    }
+
+    public static final String COMPANY_STOCKS = "companyStocks";
+    public static final String SYMBOL = "symbol";
+    public static final String NAME = "name";
+    public static final String LOGO = "logo";
+    public static final String CURRENCY = "currency";
+    public static final String CURRENCY_NAME = "currencyName";
+    public static final String SHARE_PRICE = "sharePrice";
+    public static final String NUMBER_OF_SHARES = "numberOfShares";
+}
---
+++ src/java/com/bizblock/company/CompanyStockDAO.java
@@ -0,0 +1,99 @@
+/*
+ * Copyright (c) 2018, Xyneex Technologies. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * You are not meant to edit or modify this source code unless you are
+ * authorized to do so.
+ *
+ * Please contact Xyneex Technologies, #1 Orok Orok Street, Calabar, Nigeria.
+ * or visit www.xyneex.com if you need additional information or have any
+ * questions.
+ */
+package com.bizblock.company;
+
+import static com.bizblock.company.CompanyStock.COMPANY_STOCKS;
+import com.bizblock.company.database.DBConfiguration;
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
+import java.util.Currency;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+/**
+ *
+ * @author jevis
+ * @since Mar 13, 2023 9:05:20 AM
+ */
+public class CompanyStockDAO
+{
+    public static void insertCompanyStock(CompanyStock companyStock) throws Exception
+    {
+        try( DBConfiguration dbConfig = new DBConfiguration())
+        {
+            EntityManager em = dbConfig.getEntityManager();
+            em.getTransaction().begin();
+            em.persist(companyStock);
+            em.getTransaction().commit();
+        }
+    }
+
+    public static List<CompanyStock> getAllCompanyStocks() throws Exception
+    {
+        try( DBConfiguration dbConfig = new DBConfiguration())
+        {
+            EntityManager em = dbConfig.getEntityManager();
+            String sql = "SELECT * FROM " + COMPANY_STOCKS;
+            Query q = em.createNativeQuery(sql, CompanyStock.class);
+            List<CompanyStock> companyStocks = q.getResultList();
+            return companyStocks;
+        }
+    }
+
+    public static CompanyStock getCompanyStock(String symbol) throws Exception
+    {
+        try( DBConfiguration dbConfig = new DBConfiguration())
+        {
+            EntityManager em = dbConfig.getEntityManager();
+            CompanyStock companyStock = em.find(CompanyStock.class, symbol);
+            return companyStock;
+        }
+    }
+
+    public static void updateCompanyStock(CompanyStock companyStock) throws Exception
+    {
+        try( DBConfiguration dbConfig = new DBConfiguration())
+        {
+            EntityManager em = dbConfig.getEntityManager();
+            em.getTransaction().begin();
+            CompanyStock cs = em.find(CompanyStock.class, companyStock.getSymbol());
+            cs.setNumberOfShares(companyStock.getNumberOfShares());
+            em.getTransaction().commit();
+        }
+    }
+
+    public static void loadCompanyStocks() throws Exception
+    {
+        HttpResponse<JsonNode> jsonResponse = Unirest.get("https://cloud.iexapis.com/stable/stock/market/list/mostactive")
+                .queryString("token", "pk_41d531e4fdaa4dd8b75a2d0603937581").asJson();
+        JsonNode jsonNode = jsonResponse.getBody();
+        JSONArray companiesArray = jsonNode.getArray();
+        for(int i = 0; i < companiesArray.length(); i++)
+        {
+            JSONObject jSONObject = companiesArray.getJSONObject(i);
+            CompanyStock companyStock = new CompanyStock();
+            companyStock.setSymbol(jSONObject.getString("symbol"));
+            companyStock.setName(jSONObject.getString("companyName"));
+            companyStock.setSharePrice(jSONObject.getDouble("latestPrice"));
+            companyStock.setCurrencyName(jSONObject.getString("currency"));
+            companyStock.setNumberOfShares(jSONObject.getInt("previousVolume"));
+            companyStock.setCurrency(Currency.getInstance(jSONObject.getString("currency")).getSymbol());
+            companyStock.setLogo("");
+            System.out.println("Company Inserting...: " + jSONObject.getString("companyName"));
+            insertCompanyStock(companyStock);
+        }
+    }
+}
